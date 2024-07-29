efa74ac7a7de4c58b19832e98434cfa002cc304c
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-07-29 13:26:10 +0000

    data type

--- 01_Basic/02_data_type.js
+++ 01_Basic/02_data_type.js
@@ -1,2 +1,20 @@
 "use strict"; //treat all JS code newer version
-alert()
+// alert() //we are using node js not in broweser
+
+console.log(3)
+console.log("i am suraj")//code readability is high//documentation read on //ECmaScript,tc39 ,mdn
+
+let name ="suraj"
+let age =18
+let islogin=false
+let state=null
+/*primitive*/
+//number =>2 power 53
+//bigint 
+//string=>"",''
+//boolean=>true or false
+//null=>stand alone value 
+//undefined =>value no defined
+//symbol => uniqueness
+/*object */
+console.log(typeof undefined)//null is the object and undefined is the undefined 
