8597d09d8f6371473782ed180028f280e7bb4ac4
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-07-29 11:15:42 +0000

    container config done

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,12 @@
+const account_id =144523
+let account_email="suraj@gmail.com" // must be used let because of issue in block and functional scope
+var account_password="12345"//// let use for
+accountCity="shevale" // not prefer but it is another way to declared the variable 
+let accountState;
+
+// account_id=2 // not changed
+console.log(account_id);
+account_email="akash@gamil"
+account_password="ewr"
+accountCity="chandgad"
+console.table([account_id,account_email,account_password,accountCity,accountState])
