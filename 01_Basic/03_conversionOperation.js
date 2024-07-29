e177318ca22046fc1a880dd394b8f0588bd5f894
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-07-29 13:41:01 +0000

    conversionofdatatype

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score =true
+console.log(typeof score)
+console.log(typeof (score))
+let valueInnumber=Number(score)
+// console.log(typeof (valueInnumber))
+// console.log(valueInnumber)
+// console.log(typeof NaN)
+//"33"=>33
+//"33abc"=>NaN not a number
+//
+let islogIn=1
+let booleanIsLoggedin=Boolean(islogIn)
+console.log(typeof (booleanIsLoggedin))
+
+let someNumber=33
+let stringnum=String(someNumber)
+console.log(typeof stringnum)
