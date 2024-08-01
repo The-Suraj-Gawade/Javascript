8ad12718407a8ff8cba4579504c2e768880e2e56
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-08-01 12:35:10 +0000

    maths and num

---
+++ 01_Basic/06_Num_Maths.js
@@ -0,0 +1,29 @@
+const score=400
+const balance =new Number (100)//consider as the number
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//to fix precision value
+const number=23.556
+// console.log(number.toPrecision(3));
+
+const Hundred =1000000
+// console.log(Hundred.toLocaleString("en-IN"));//convert number to easy to read
+// console.log(Number.MAX_SAFE_INTEGER);
+
+//+++++++++++++++++++maths+++++++++++++++++++++
+console.log(Math);
+
+// console.log((Math.abs(-4)));
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(4.67));
+// console.log(Math.floor(4.67));
+// console.log(Math.sqrt(4));
+
+console.log(Math.random());//give value betn 0 to 1
+console.log((Math.floor(Math.random()*10)+1));
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
