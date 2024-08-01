ed97ce222f9df1eeb6f33618bf36535f84394fc3
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-08-01 12:55:47 +0000

    date and time

---
+++ 01_Basic/07_datein.js
@@ -0,0 +1,23 @@
+//Dates
+
+ let myDate = new Date("08-01-2024")
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof(myDate));
+
+// myCreatedDate=new Date (2024,0,1,5,8,9,5,4)
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+console.log(newDate)
+console.log(newDate.getMonth());
+
+`${newDate.getDate()}`
+
+
