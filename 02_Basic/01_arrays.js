46b9867b972486c4ff59af5f85da269f19d7be8d
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-08-02 15:57:45 +0000

    arrays basics

--- 02_Basic/01_arrays.js
+++ 02_Basic/01_arrays.js
@@ -1,3 +1,32 @@
 // array
 const myArray=[0,1,2,3,4,5,"suraj"]
-console.log(myArray[0]);
+// console.log(myArray[0]);
+
+// const Myarray=new Array(1,2,3,4,5)
+// console.log(Myarray);
+
+// myArray.push(6)
+// myArray.unshift(9);
+// myArray.shift()//make to the program slow it is not optimize
+// console.log(myArray);
+// console.log(myArray.includes(4))
+// console.log(myArray.indexOf(4))
+
+// const newArr=myArray.join()//change into the string
+// console.log(typeof newArr)
+
+//slice ,splice
+
+console.log("A ",myArray);
+
+const myn1 =myArray.slice(1,3)
+console.log(myn1);
+console.log("B ",myArray);
+
+const myn2 =myArray.splice(1,3)
+console.log("c ",myArray);
+console.log(myn2);
+
+
+
+
