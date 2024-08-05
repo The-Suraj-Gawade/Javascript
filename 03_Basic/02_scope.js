286ab701889c52db83f8ccbd736ffaf59d5bb270
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-08-05 12:23:25 +0000

    scope

---
+++ 03_Basic/02_scope.js
@@ -0,0 +1,23 @@
+
+// let a=30000
+// if(true){// let a=10
+// // const b=20
+// var c =30 // var having the in all program having global scope 
+// }
+
+// // console.log();
+// // console.log();
+// console.log(c);
+
+function one(){
+    const username ="suraj"
+}
+function two (){
+    const website="youtube"
+    console.log(website);
+}
+// console.log(username);
+two()
+
+
+
