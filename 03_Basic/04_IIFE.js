9e2b212fb4160ae2a25da43584504662baeb2127
The-Suraj-Gawade <csemr.surajgawade@gmail.com>
2024-08-06 12:57:26 +0000

    execute code

--- 03_Basic/04_IIFE.js
+++ 03_Basic/04_IIFE.js
@@ -19,16 +19,38 @@
 
 /*Javascript Execution Contact
 
-1st ->execute global ececution context
+1st ->execute global ececution context//Always made 
+->#javascript is single threaded
+
 ->Global Execution Constaxt:
 
 ->Function Execution Context:
 
-->Eval Execution context:
+->Eval Execution context:used in the eval mangus
 
 phases:
 ->memory creation phase/creation phase
 ->Execution phase
 ->
+
+#Steps of the js code execution#
+1)Global Execuiton
+    ->allocated in the this pointer / only this 
+
+2)Memory Phase:
+    ->all variable declared as undefined
+    ->if function is there in the name of the function contain the function def
+
+3)Execution Phase:
+    ->allocation all variable to the values if there given
+    ->if there is function then create a new Execution context /threads
+        ->if function call is occured then again impliment the step2 and step 3
+        ->function return value to Global Execution contect
+    ->global execution contact will delete after the execution
+
+Call Stack:
+    Global EC add in the stack always
+    
+
 
 */
